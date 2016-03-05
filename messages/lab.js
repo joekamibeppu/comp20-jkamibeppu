46d1027f51cbad980e6dee7c3f31743782315203
Joe Kamibeppu <joekamibeppu@gmail.com>
2016-03-05 11:46:36 -0500

    lab.js draft

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,17 @@
-// Your JavaScript goes here...
+function parse() {
+
+	var request = new XMLHttpRequest();
+	var data = "data.json";
+
+	request.open("GET", data, true);
+
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			var lyrics = JSON.parse(request.responseText);
+			document.getElementByID("lyrics").innerHTML =
+			lyrics[0].content + lyrics[1].content;
+		}
+	}
+
+	request.send();
+}

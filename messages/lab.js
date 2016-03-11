daa032f555830fde3ab2bd16ae85723e585aec6a
Joe Kamibeppu <joekamibeppu@gmail.com>
2016-03-10 20:30:37 -0500

    created landmarks

--- messages/lab.js
+++ messages/lab.js
@@ -15,6 +15,7 @@ function parse() {
 	request.onreadystatechange = function() {
 		if (request.readyState == 4 && request.status == 200) {
 			var messages = JSON.parse(request.responseText);
+			
 			document.getElementById("messages").innerHTML = 
 			messages[0].content + "\xa0" + messages[0].username + "<br>" 
 			+ "<br>" + 
